089346d5dff3e221cb733b67c5388bd798e4a323
premchand-03 <premchandsah8292131208@gmail.com>
2026-09-11 19:59:24 +0530

    Improve application security

--- delete_student.php
+++ delete_student.php
@@ -2,20 +2,27 @@
 
 include "db.php";
 
-if (isset($_GET['id'])) {
-
-    $id = $_GET['id'];
-
-    $sql = "DELETE FROM students WHERE id = ?";
-
-    $stmt = mysqli_prepare($conn, $sql);
-
-    mysqli_stmt_bind_param($stmt, "i", $id);
-
-    mysqli_stmt_execute($stmt);
-
-    mysqli_stmt_close($stmt);
+if (!isset($_GET["id"])) {
+    header("Location: students.php");
+    exit;
 }
+
+$id = (int) $_GET["id"];
+
+if ($id <= 0) {
+    header("Location: students.php");
+    exit;
+}
+
+$sql = "DELETE FROM students WHERE id = ?";
+
+$stmt = mysqli_prepare($conn, $sql);
+
+mysqli_stmt_bind_param($stmt, "i", $id);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
 
 header("Location: students.php");
 exit;
